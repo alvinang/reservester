d3636f6a3307eb48d8289536279adeab117ef934
IdealEthos <idealethos@gmail.com>
2014-03-14 14:13:39 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reservester::Application.config.secret_key_base = 'ef129f3db52ca7876507b0f941b6f8da1ed61ffcbe566ca196c9035cf42afc0870aaa70dcfa1f2d52926ea811765b64419996ebd749b6efcccbdd223394dbcea'
