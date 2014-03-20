64111c4a99305cb959faa22b293c832cc9cf6852
IdealEthos <idealethos@gmail.com>
2014-03-19 23:07:42 -0400

    IONO Man...

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -18,6 +18,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
 
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
