0ea73202021cb3fb0bf4f0d06ac3788d2d801d83
IdealEthos <idealethos@gmail.com>
2014-03-19 21:09:55 -0400

    Fix CarrierWave-Heroku Cache

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,5 +4,8 @@ CarrierWave.configure do |config|
 		:aws_access_key_id		=> ENV['S3_Access_Key'],
 		:aws_secret_access_key	=> ENV['S3_Secret_Key'],
 	}
+	
 	config.fog_directory = ENV['S3_Bucket']
+
+	config.cache_dir = "#{Rails.root}/tmp/uploads"
 end
